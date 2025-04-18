77a235c41d06be8813e117235414c46cfaf457e4
madhava077 <lvsmadhava@gmail.com>
2025-04-18 18:29:22 +0530

    Initial commit

---
+++ src/main/java/com/shopverse/shopverse/ShopverseApplication.java
@@ -0,0 +1,13 @@
+package com.shopverse.shopverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopverseApplication.class, args);
+	}
+
+}
