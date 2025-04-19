af8c3105b344d6733c40310f0e28cc1e934217c4
madhava077 <lvsmadhava@gmail.com>
2025-04-19 12:44:42 +0530

    productmodel

---
+++ src/main/java/com/shopverse/shopverse/model/Product.java
@@ -0,0 +1,72 @@
+package com.shopverse.shopverse.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="productname")
+    private String productname;
+    @Column(name="description")
+    private String description;
+    @Column(name="price")
+    private double price;
+    @Column(name="stock")
+    private int stock;
+    @Column(name="category")
+    private String category;
+    public Product() {
+    }
+    public Product(Long id, String productname, String description, double price, int stock, String category) {
+        this.id = id;
+        this.productname = productname;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductname() {
+        return productname;
+    }
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
