19e9bc318b50f914a3329555c372c08cedb576a1
ANUDEEP-SINGH-NELLORE <nelloreanudeepanand@gmail.com>
2025-04-19 12:28:37 +0530

    user.2

--- src/main/java/com/shopverse/shopverse/model/user.java
+++ src/main/java/com/shopverse/shopverse/model/user.java
@@ -1,5 +1,60 @@
 package com.shopverse.shopverse.model;
 
 public class user {
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+    private String address;
 
+    public user() {
+    }
+
+    public user(String username, String password, String email, String fullName, String address) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
