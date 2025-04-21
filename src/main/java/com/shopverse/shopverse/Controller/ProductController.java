0b786abfb9e5e1229f9de4841b040a447e1751e1
madhava077 <lvsmadhava@gmail.com>
2025-04-21 13:07:31 +0530

    ProductController

--- src/main/java/com/shopverse/shopverse/Controller/ProductController.java
+++ src/main/java/com/shopverse/shopverse/Controller/ProductController.java
@@ -1,5 +1,46 @@
 package com.shopverse.shopverse.Controller;
 
-public class ProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.shopverse.shopverse.Dto.ProductDto;
+import com.shopverse.shopverse.Service.ProductService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @PostMapping("/add")
+    public ResponseEntity<ProductDto> addProduct(@RequestBody ProductDto productDto) {
+        ProductDto createdProduct = productService.createProduct(productDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
+        ProductDto product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        ProductDto updatedProduct = productService.updateProduct(id, productDto);
+        return ResponseEntity.ok(updatedProduct);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
 }
--- src/main/java/com/shopverse/shopverse/Service/ProductService.java
+++ src/main/java/com/shopverse/shopverse/Service/ProductService.java
@@ -1,5 +1,12 @@
 package com.shopverse.shopverse.Service;
 
+import com.shopverse.shopverse.Dto.ProductDto;
+import com.shopverse.shopverse.model.Product;
+
 public interface ProductService {
-    
+    public ProductDto createProduct(ProductDto productDto);
+    public ProductDto getProductById(Long id);
+    public ProductDto updateProduct(Long id, ProductDto productDto);
+    public void deleteProduct(Long id);
+    public Product getProductByIdReturnProduct(Long id);
 }
