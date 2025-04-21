011baaec1a62a178c07c426b20cbc5ee56f2e57d
madhava077 <lvsmadhava@gmail.com>
2025-04-21 15:13:43 +0530

    errorfixed

--- src/main/java/com/shopverse/shopverse/Repository/UserRepository.java
+++ src/main/java/com/shopverse/shopverse/Repository/UserRepository.java
@@ -1,17 +1,11 @@
 package com.shopverse.shopverse.Repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopverse.shopverse.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	Optional<User> findByEmail(String email);
-    Optional<User> findByPhone(String phone);
-    Optional<User> findByUsername(String username);
-    Optional<User> findByEmailAndPassword(String email, String password);
-    Optional<User> findByPhoneAndPassword(String phone, String password);
-    Optional<User> findByUsernameAndPassword(String username, String password);
+	
 }
 
