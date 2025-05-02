90b8ee525d4592455b22a831e9efbbbd117c2440
madhava077 <lvsmadhava@gmail.com>
2025-05-02 10:28:28 +0530

    frontend added

--- src/main/java/com/shopverse/shopverse/WebConfig.java
+++ src/main/java/com/shopverse/shopverse/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500"); // Adjust the frontend URL
+        registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500");
     }
 }
