dc862c3196134c89a18a6574f85168a2825800c0
madhava077 <lvsmadhava@gmail.com>
2025-06-16 13:27:44 +0530

    Docker

--- src/main/java/com/shopverse/shopverse/CorsConfig.java
+++ src/main/java/com/shopverse/shopverse/CorsConfig.java
@@ -13,8 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost", "http://127.0.0.1:5500")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
